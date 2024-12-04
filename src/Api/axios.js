6f22744e98edde005eee6872bc20f94d09c51612
Coder Miki <mikiastadessemiki@gmail.com>
2024-12-04 22:22:10 +0300

    configure the backened url

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.0.109:5000/",
+  // localhost address
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://amazon-backend-api-71jy.onrender.com/",
 });
 
 export { axiosInstance };
